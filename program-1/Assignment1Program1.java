abe154adf818a5a292d5194243baf3ca1194f5a5
ellasahin <sahin@hartford.edu>
2025-09-17 15:41:45 -0400

    Update Assignment1Program1.java

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,35 @@
-public class Assignment1Program1 {
-  public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Assignment1Program1 {
+  public static void main(String[] args){
+        System.out.println("EEEEE  SSSSS");
+        System.out.println("E      S");
+        System.out.println("EEEEE  SSSSS");
+        System.out.println("E          S");
+        System.out.println("EEEEE  SSSSS");
+
+        System.out.println();
   }
+
+public static void second(String[] args){
+
+          Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a 5-character string: ");
+        String input = sc.nextLine();
+
+        if (input.length() == 5) {
+
+            String trimmed = input.substring(1, 4);
+
+            String reversed = new StringBuilder(trimmed).reverse().toString();
+
+            System.out.println("Result: " + reversed);
+        } else {
+            System.out.println("Please enter exactly 5 characters.");
+        }
+            System.out.println();
+        sc.close();
+
+    }
 }
